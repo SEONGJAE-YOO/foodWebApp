b9d0e8ef72383bb5659ec6bea21cb649e329c0f2
YooSeongJae <54341259+SEONGJAE-YOO@users.noreply.github.com>
2019-11-08 11:13:11 +0900

    Add files via upload

---
+++ WebContent/jsonFlickrFeed.js
@@ -0,0 +1,141 @@
+jsonFlickrFeed({
+		"title": "Uploads from hmaps.image, tagged mdlife",
+		"link": "https:\/\/www.flickr.com\/photos\/137506863@N06\/tags\/mdlife\/",
+		"description": "",
+		"modified": "2015-11-01T15:30:59Z",
+		"generator": "https:\/\/www.flickr.com",
+		"items": [
+	   {
+			"title": "오근내닭갈비",
+			"link": "http://localhost:8011/foodapp/a.jpg",
+			"media": {"m":"http://localhost:8011/foodapp/a.jpg"},
+			"date_taken": "2015-11-01T07:30:59-08:00",  
+			"description": " <p><a href=\"https:\/\/www.flickr.com\/people\/137506863@N06\/\">hmaps.image<\/a> posted a photo:<\/p> <p><a href=\"https:\/\/www.flickr.com\/photos\/137506863@N06\/22663129542\/\" title=\"\uccad\ucd98\ud50c\ub7ab\ud3fc_\uc6b0\uc218\ubd80\uc2a4\ud22c\ud45c\"><img src=\"a.jpg\" width=\"240\" height=\"135\" alt=\"\uccad\ucd98\ud50c\ub7ab\ud3fc_\uc6b0\uc218\ubd80\uc2a4\ud22c\ud45c\" \/><\/a><\/p> ",
+			"published": "2015-11-01T15:30:59Z",
+			"author": "nobody@flickr.com (\"a.jpg\")",
+			"author_id": "137506863@N06",
+			"tags": "mdlife"
+	   },
+	   {
+			"title": "마음집",
+			"link": "http://localhost:8011/foodapp/b.jpg",
+			"media": {"m":"http://localhost:8011/foodapp/b.jpg"},
+			"date_taken": "2015-11-01T07:30:58-08:00",
+			"description": " <p><a href=\"https:\/\/www.flickr.com\/people\/137506863@N06\/\">hmaps.image<\/a> posted a photo:<\/p> <p><a href=\"https:\/\/www.flickr.com\/photos\/137506863@N06\/22650669476\/\" title=\"\uc878\uc5c5\uc791\ud488\uc804\uc2dc\ud68c \uc8fc\uc81c\"><img src=\"b.jpg\" width=\"240\" height=\"160\" alt=\"\uc878\uc5c5\uc791\ud488\uc804\uc2dc\ud68c \uc8fc\uc81c\" \/><\/a><\/p> ",
+			"published": "2015-11-01T15:30:58Z",
+			"author": "nobody@flickr.com (\"b.jpg\")",
+			"author_id": "137506863@N06",
+			"tags": "mdlife"
+	   },
+	   {
+			"title": "장군집",
+			"link": "http://localhost:8011/foodapp/c.jpg",
+			"media": {"m":"http://localhost:8011/foodapp/c.jpg"},
+			"date_taken": "2015-11-01T07:30:58-08:00",
+			"description": " <p><a href=\"https:\/\/www.flickr.com\/people\/137506863@N06\/\">hmaps.image<\/a> posted a photo:<\/p> <p><a href=\"https:\/\/www.flickr.com\/photos\/137506863@N06\/22663128752\/\" title=\"\uc804\uc2dc\uc7a5\ubaa8\uc2b5\"><img src=\"c.jpg\" width=\"240\" height=\"135\" alt=\"\uc804\uc2dc\uc7a5\ubaa8\uc2b5\" \/><\/a><\/p> ",
+			"published": "2015-11-01T15:30:58Z",
+			"author": "nobody@flickr.com (\"c.jpg\")",
+			"author_id": "137506863@N06",
+			"tags": "mdlife"
+	   },
+	   {
+			"title": "루프탑 클라우드",
+			"link": "http://localhost:8011/foodapp/d.jpg",
+			"media": {"m":"http://localhost:8011/foodapp/d.jpg"},
+			"date_taken": "2015-11-01T07:30:57-08:00",
+			"description": " <p><a href=\"https:\/\/www.flickr.com\/people\/137506863@N06\/\">hmaps.image<\/a> posted a photo:<\/p> <p><a href=\"https:\/\/www.flickr.com\/photos\/137506863@N06\/22055529403\/\" title=\"pc\uc2e4\uc2b5\uc2e4\"><img src=\"d.jpg\" width=\"240\" height=\"160\" alt=\"pc\uc2e4\uc2b5\uc2e4\" \/><\/a><\/p> ",
+			"published": "2015-11-01T15:30:57Z",
+			"author": "nobody@flickr.com (\"d.jpg\")",
+			"author_id": "137506863@N06",
+			"tags": "mdlife"
+	   },
+	   {
+			"title": "소두마리",
+			"link": "http://localhost:8011/foodapp/e.jpg",
+			"media": {"m":"http://localhost:8011/foodapp/e.jpg"},
+			"date_taken": "2015-11-01T07:30:58-08:00",
+			"description": " <p><a href=\"https:\/\/www.flickr.com\/people\/137506863@N06\/\">hmaps.image<\/a> posted a photo:<\/p> <p><a href=\"https:\/\/www.flickr.com\/photos\/137506863@N06\/22650669326\/\" title=\"\uc804\uc2dc_\ubc29\uba85\ub85d\"><img src=\"e.jpg\" width=\"160\" height=\"240\" alt=\"\uc804\uc2dc_\ubc29\uba85\ub85d\" \/><\/a><\/p> ",
+			"published": "2015-11-01T15:30:58Z",
+			"author": "nobody@flickr.com (\"e.jpg\")",
+			"author_id": "137506863@N06",
+			"tags": "mdlife"
+	   },
+	   {
+			"title": "히네꼬",
+			"link": "http://localhost:8011/foodapp/f.jpg",
+			"media": {"m":"http://localhost:8011/foodapp/f.jpg"},
+			"date_taken": "2015-11-01T07:30:58-08:00",
+			"description": " <p><a href=\"https:\/\/www.flickr.com\/people\/137506863@N06\/\">hmaps.image<\/a> posted a photo:<\/p> <p><a href=\"f.jpg\" width=\"240\" height=\"161\" alt=\"\uba85\ud568\ub514\uc790\uc778\" \/><\/a><\/p> ",
+			"published": "2015-11-01T15:30:58Z",
+			"author": "nobody@flickr.com (\"f.jpg\")",
+			"author_id": "137506863@N06",
+			"tags": "mdlife"
+	   },
+	   {
+			"title": "고운님한우곱창",
+			"link": "http://localhost:8011/foodapp/g.jpg",
+			"media": {"m":"http://localhost:8011/foodapp/g.jpg"},
+			"date_taken": "2015-11-01T07:30:57-08:00",
+			"description": " <p><a href=\"https:\/\/www.flickr.com\/people\/137506863@N06\/\">hmaps.image<\/a> posted a photo:<\/p> <p><a href=\"https:\/\/www.flickr.com\/photos\/137506863@N06\/22687855771\/\" title=\"\ub9e5 \uc2e4\uc2b5\uc2e4\"><img src=\"g.jpg\" width=\"240\" height=\"160\" alt=\"\ub9e5 \uc2e4\uc2b5\uc2e4\" \/><\/a><\/p> ",
+			"published": "2015-11-01T15:30:57Z",
+			"author": "nobody@flickr.com (\"g.jpg\")",
+			"author_id": "137506863@N06",
+			"tags": "mdlife"
+	   },
+	   {
+			"title": "원조아구해물찜",
+			"link": "http://localhost:8011/foodapp/h.jpg",
+			"media": {"m":"http://localhost:8011/foodapp/h.jpg"},
+			"date_taken": "2015-11-01T07:30:58-08:00",
+			"description": " <p><a href=\"https:\/\/www.flickr.com\/people\/137506863@N06\/\">hmaps.image<\/a> posted a photo:<\/p> <p><a href=\"https:\/\/www.flickr.com\/photos\/137506863@N06\/22053918354\/\" title=\"\uce98\ub9ac\uadf8\ub77c\ud53c_\ub514\uc790\uc778\"><img src=\"h.jpg\" width=\"240\" height=\"160\" alt=\"\uce98\ub9ac\uadf8\ub77c\ud53c_\ub514\uc790\uc778\" \/><\/a><\/p> ",
+			"published": "2015-11-01T15:30:58Z",
+			"author": "nobody@flickr.com (\"h.jpg\")",
+			"author_id": "137506863@N06",
+			"tags": "mdlife"
+	   },
+	   {
+			"title": "청자골",
+			"link": "http://localhost:8011/foodapp/i.jpg",
+			"media": {"m":"http://localhost:8011/foodapp/i.jpg"},
+			"date_taken": "2015-11-01T07:30:58-08:00",
+			"description": " <p><a href=\"https:\/\/www.flickr.com\/people\/137506863@N06\/\">hmaps.image<\/a> posted a photo:<\/p> <p><a href=\"https:\/\/www.flickr.com\/photos\/137506863@N06\/22687856291\/\" title=\"\uc2e4\uc2b5\uc2e4\ubcf5\ub3c4\"><img src=\"i.jpg\" width=\"240\" height=\"160\" alt=\"\uc2e4\uc2b5\uc2e4\ubcf5\ub3c4\" \/><\/a><\/p> ",
+			"published": "2015-11-01T15:30:58Z",
+			"author": "nobody@flickr.com (\"i.jpg\")",
+			"author_id": "137506863@N06",
+			"tags": "mdlife"
+	   },
+	   {
+			"title": "고메고매",
+			"link": "http://localhost:8011/foodapp/j.jpg",
+			"media": {"m":"http://localhost:8011/foodapp/j.jpg"},
+			"date_taken": "2015-11-01T07:30:57-08:00",
+			"description": " <p><a href=\"https:\/\/www.flickr.com\/people\/137506863@N06\/\">hmaps.image<\/a> posted a photo:<\/p> <p><a href=\"https:\/\/www.flickr.com\/photos\/137506863@N06\/22055529593\/\" title=\"\uc778\ud130\ubdf0_\ucd2c\uc601\"><img src=\"j.jpg\" width=\"240\" height=\"160\" alt=\"\uc778\ud130\ubdf0_\ucd2c\uc601\" \/><\/a><\/p> ",
+			"published": "2015-11-01T15:30:57Z",
+			"author": "nobody@flickr.com (\"j.jpg\")",
+			"author_id": "137506863@N06",
+			"tags": "mdlife"
+	   },
+	   {
+			"title": "수림복국",
+			"link": "http://localhost:8011/foodapp/k.jpg",
+			"media": {"m":"http://localhost:8011/foodapp/k.jpg"},
+			"date_taken": "2015-11-01T07:30:57-08:00",
+			"description": " <p><a href=\"https:\/\/www.flickr.com\/people\/137506863@N06\/\">hmaps.image<\/a> posted a photo:<\/p> <p><a href=\"https:\/\/www.flickr.com\/photos\/137506863@N06\/22053918014\/\" title=\"\uc804\uc2dc_\uba54\ubaa8\"><img src=\"k.jpg\" width=\"240\" height=\"135\" alt=\"\uc804\uc2dc_\uba54\ubaa8\" \/><\/a><\/p> ",
+			"published": "2015-11-01T15:30:57Z",
+			"author": "nobody@flickr.com (\"k.jpg\")",
+			"author_id": "137506863@N06",
+			"tags": "mdlife"
+	   },
+	   {  
+			"title": "진미닭곰탕",
+			"link": "http://localhost:8011/foodapp/kk.jpg",
+			"media": {"m":"http://localhost:8011/foodapp/kk.jpg"},
+			"date_taken": "2015-11-01T07:30:58-08:00",
+			"description": " <p><a href=\"https:\/\/www.flickr.com\/people\/137506863@N06\/\">hmaps.image<\/a> posted a photo:<\/p> <p><a href=\"https:\/\/www.flickr.com\/photos\/137506863@N06\/22055530083\/\" title=\"\uc885\uc774\ucef5_\uce98\ub9ac\uadf8\ub798\ud53c\"><img src=\"kk.jpg\" width=\"240\" height=\"135\" alt=\"\uc885\uc774\ucef5_\uce98\ub9ac\uadf8\ub798\ud53c\" \/><\/a><\/p> ",
+			"published": "2015-11-01T15:30:58Z",
+			"author": "nobody@flickr.com (\"kk.jpg\")",
+			"author_id": "137506863@N06",
+			"tags": "mdlife"
+	   }
+        ]
+})
